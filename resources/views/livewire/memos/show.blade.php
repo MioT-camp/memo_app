3f60a0557644881ab257551ee11395541aed5647
MioT-camp <taitaihanahana@gmail.com>
2025-08-31 14:32:02 +0900

    削除機能の完成

--- resources/views/livewire/memos/show.blade.php
+++ resources/views/livewire/memos/show.blade.php
@@ -6,8 +6,13 @@ use App\Models\Memo;
 // ルートモデルバインディング
 state(['memo' => fn(Memo $memo) => $memo]);
 
-$edit = function() {
+$edit = function () {
     return redirect()->route('memos.edit', $this->memo);
+};
+
+$destroy = function () {
+    $this->memo->delete();
+    return redirect()->route('memos.index');
 };
 
 ?>
@@ -18,4 +23,5 @@ $edit = function() {
     <p>{!! nl2br(e($memo->body)) !!}</p>
 
     <button wire:click="edit">編集する</button>
+    <button wire:click="destroy" wire:confirm="本当に削除しますか？">削除する</button>
 </div>
