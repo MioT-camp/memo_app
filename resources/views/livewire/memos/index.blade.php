32f49f0dc72a047e78e7c1f8dcca83e270a2f7c8
MioT-camp <taitaihanahana@gmail.com>
2025-09-05 14:15:36 +0900

    feat: 優先度順ソート機能と色分け表示機能を追加

--- resources/views/livewire/memos/index.blade.php
+++ resources/views/livewire/memos/index.blade.php
@@ -3,19 +3,28 @@
 use function Livewire\Volt\{state};
 use App\Models\Memo;
 
-state(['memos' => fn() => Memo::all()]);
+state(['memos' => fn() => Memo::orderBy('priority', 'desc')->get()]);
 
 $create = function () {
     return redirect()->route('memos.create');
 };
 
-
+// 優先度を文字列に変換する関数
 $getPriorityText = function ($priority) {
     return match ($priority) {
         1 => '低',
         2 => '中',
         3 => '高',
         default => '不明',
+    };
+};
+
+// 優先度に応じたCSSクラスを取得する関数
+$getPriorityClass = function ($priority) {
+    return match ($priority) {
+        1 => 'priority-low',
+        2 => 'priority-medium',
+        3 => 'priority-high',
     };
 };
 
@@ -27,7 +36,7 @@ $getPriorityText = function ($priority) {
         @foreach ($memos as $memo)
             <li>
                 <a href="{{ route('memos.show', $memo) }}">
-                    {{ $memo->title }}[{{ $this->getPriorityText($memo->priority) }}]
+                    {{ $memo->title }}[<span class="{{ $this->getPriorityClass($memo->priority) }}">{{ $this->getPriorityText($memo->priority) }}</span>]
                 </a>
             </li>
         @endforeach
